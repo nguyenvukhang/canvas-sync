0f85c1525fd149a7f592ec44a48ec35765945f6b
nguyenvukhang <brew4k@gmail.com>
2023-01-30 17:23:29 +0800

    drop eprintln

--- csrv/types.cc
+++ csrv/types.cc
@@ -1,14 +1,6 @@
 #include "types.h"
 #include <cstdarg>
 
-void eprintln(const char *fmt, ...)
-{
-  va_list args;
-  va_start(args, fmt);
-  vfprintf(stderr, fmt, args);
-  std::cerr << std::endl;
-}
-
 std::string normalize_filename(const char *a, size_t size)
 {
   std::string result = "";
@@ -36,30 +28,30 @@ std::string normalize_filename(std::string *v)
 
 void debug(Profile *p)
 {
-  eprintln("Profile");
-  eprintln("* id:         %d", p->id);
-  eprintln("* name:       %s", p->name.c_str());
-  eprintln("* email:      %s", p->primary_email.c_str());
-  eprintln("* login_id:   %s", p->login_id.c_str());
-  eprintln("* student_id: %s", p->integration_id.c_str());
+  std::cout << "Profile" << std::endl;
+  printf("* id:         %d\n", p->id);
+  printf("* name:       %s\n", p->name.c_str());
+  printf("* email:      %s\n", p->primary_email.c_str());
+  printf("* login_id:   %s\n", p->login_id.c_str());
+  printf("* student_id: %s\n", p->integration_id.c_str());
 }
 void debug(Course *c)
 {
-  eprintln("Course");
-  eprintln("* id:   %d", c->id);
-  eprintln("* name: %s", c->name.c_str());
+  std::cout << "Course" << std::endl;
+  printf("* id:   %d\n", c->id);
+  printf("* name: %s\n", c->name.c_str());
 }
 void debug(File *c)
 {
-  eprintln("File { %d, %s }", c->id, c->filename.c_str());
+  printf("File { %d, %s }\n", c->id, c->filename.c_str());
 }
 void debug(Update *c)
 {
-  eprintln("Update { folder_id: %d, local_dir: %s }", c->folder_id,
+  printf("Update { folder_id: %d, local_dir: %s }\n", c->folder_id,
            c->local_dir.string().c_str());
 }
 void debug(Folder *c)
 {
-  eprintln("Folder { %d, %s, (%s) }", c->id, c->full_name.c_str(),
+  printf("Folder { %d, %s, (%s) }\n", c->id, c->full_name.c_str(),
            c->name.c_str());
 }
