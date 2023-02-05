30a6f5fb49f1d415fc858abeea455833556428b6
nguyenvukhang <brew4k@gmail.com>
2023-02-06 01:01:48 +0800

    fix: github actions build

--- main.cc
+++ main.cc
@@ -2,13 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-  Profile p;
-  p.id = 1000;
-  std::cout << p.id << std::endl;
-  return 0;
-  // QApplication a(argc, argv);
-  // std::cout << "STARTING PRODCTION APP" << std::endl;
-  // MainWindow w(new Canvas("https://canvas.nus.edu.sg"));
-  // w.show();
-  // return a.exec();
+  QApplication a(argc, argv);
+  MainWindow w(new Canvas("https://canvas.nus.edu.sg"));
+  w.show();
+  return a.exec();
 }
