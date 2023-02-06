6f3c468ac44100540b92105a371af5ee46bed45e
nguyenvukhang <brew4k@gmail.com>
2023-02-06 11:24:10 +0800

    test: remove test configs after completion

--- test/main.h
+++ test/main.h
@@ -10,23 +10,44 @@
 #include "fake_canvas.h"
 
 #include <algorithm>
+#include <filesystem>
+#include <mutex>
+#include <vector>
 
 class TestGui : public QObject
 {
   Q_OBJECT
+  int id = 1;
+  std::mutex id_mtx;
+  std::vector<QString> tmp_settings;
 
 public:
-  TestGui() : app(new FakeCanvas(), "canvas-sync-test.ini"){};
-
-  MainWindow app;
-
   // convenience routines
-  void authenticate();
+  void authenticate(MainWindow *app);
+  MainWindow *create_app()
+  {
+    return new MainWindow(new FakeCanvas(), settings_file());
+  }
+  QString settings_file()
+  {
+    this->id_mtx.lock();
+    QString result = QString("canvas-sync-test-%1.ini").arg(this->id++);
+    tmp_settings.push_back(MainWindow::settings_path + '/' + result);
+    this->id_mtx.unlock();
+    return result;
+  }
 
 private slots: // tests
   void access_token_entry_test();
   void fetch_courses_test();
   void fetch_courses_ui_test();
+  void cleanupTestCase()
+  {
+    this->id_mtx.lock();
+    for (auto i : this->tmp_settings)
+      std::filesystem::remove(i.toStdString());
+    this->id_mtx.unlock();
+  };
 };
 
 #endif
