c1da458d11a3eec76786abd56a3575983e46f7da
nguyenvukhang <brew4k@gmail.com>
2023-02-09 17:48:35 +0800

    wipe

--- main.cc
+++
@@ -1,9 +0,0 @@
-#include "csync/canvas_sync.h"
-
-int main(int argc, char *argv[])
-{
-  QApplication a(argc, argv);
-  MainWindow w("https://canvas.nus.edu.sg");
-  w.show();
-  return a.exec();
-}
